cd102425a21500b129782c690df59bb23f0f0935
uzhojiakbar <uzhojiakbar3@gmail.com>
2023-06-01 23:18:59 +0500

    First Commit

---
+++ src/Mock/knives.js
@@ -0,0 +1,12 @@
+import img1 from '../Assets/imgs/knifes/knife_1.svg'
+import img2 from '../Assets/imgs/knifes/knife_2.svg'
+
+export const KnivesData = [
+    {
+        id: 1,
+        img: img1,
+        name: 'Складной нож SQ01-B',
+        price: { text: '850 р.', num: 850 },
+        new: true,
+    },
+]
